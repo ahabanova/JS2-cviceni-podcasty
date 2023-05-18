4e69eb4a5b91827ffc3746d3266acebbcd90958e
ahabanova <ahabanova95@gmail.com>
2023-05-18 21:05:50 +0200

    hotovo

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
+import { Podcast } from "./Podcast/index.js";
+
 const podcast1 = {
   num: '126',
   title: 'Robot, který snědl koblihu',
   guest: 'Radovan Siwek',
 };
 
+const episodes = [
+  {
+    num: 127,
+    title: 'Hledání plyšového Yettiho',
+    guest: 'Vojtěch Ryba',
+  },
+  {
+    num: 128,
+    title: 'Moderní hrachová polévka',
+    guest: 'Kamila Štancová',
+  },
+  {
+    num: 129,
+    title: 'Poloautomatické zrcadlo',
+    guest: 'Janka Janovská',
+  },
+  {
+    num: 130,
+    title: 'Máčené hlavy parlamentu',
+    guest: 'Jonáš Daněk',
+  },
+  {
+    num: 131,
+    title: 'Služby na hraně přívěsu',
+    guest: 'Tereza Křivánková',
+  },
+  {
+    num: 132,
+    title: 'Klasifikace sněžných klokanů',
+    guest: 'Josef Stix',
+  },
+  {
+    num: 133,
+    title: 'Rybolov v Oceánu bouří',
+    guest: 'Ludmila Gajová',
+  },
+];
+
 console.log('funguju!', podcast1);
+
+const episodesElm = document.querySelector('.episodes-list')
+
+episodesElm.innerHTML += Podcast(podcast1)
+
+episodesElm.innerHTML += episodes.map((item) => Podcast({
+  num: item.num,
+  title: item.title,
+  guest: item.guest
+})).join('')
